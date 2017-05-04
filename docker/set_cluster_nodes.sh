c268dbee875126b89b6a675285c3ebd77b0b8bda
Vitali D <vitali.domashkevitsh@gmail.com>
2017-05-04 22:40:43 +0300

    Update set_cluster_nodes.sh

--- docker/set_cluster_nodes.sh
+++ docker/set_cluster_nodes.sh
@@ -7,9 +7,11 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 
 STATEFUL_SETS=$(curl -f -k https://10.0.0.1/apis/apps/v1beta1/statefulsets -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)")
 RMQ_SS=$(echo $STATEFUL_SETS | jq '.items[] | select(.metadata.name == "rabbitmq")')
-REPLICAS=$(echo $RMQ_SS | jq .spec.replicas)
-SERVICE_NAME=$(echo $RMQ_SS | jq .spec.serviceName | tr -d '"')
-(( REPLICAS-= 1 ))
+#REPLICAS=$(echo $RMQ_SS | jq .spec.replicas)
+REPLICAS=2
+#SERVICE_NAME=$(echo $RMQ_SS | jq .spec.serviceName | tr -d '"')
+SERVICE_NAME=rabbitmq
+#(( REPLICAS-= 1 ))
 NODES=()
 
 for i in $(seq 0 $REPLICAS)
